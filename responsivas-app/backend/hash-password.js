1f2f43601e3642243c7f289ba4c5708d48040ba6
Javier Romo <romojavier718@gmail.com>
2025-10-17 15:10:48 -0600

    Añadir archivo de hash de contraseña y ajustar .gitignore para incluirlo

--- responsivas-app/backend/hash-password.js
+++ responsivas-app/backend/hash-password.js
@@ -1,3 +1,4 @@
+//no debe de estarse ejecutando en producción
 const bcrypt = require('bcrypt');
 const password = 'IGbiogas.2023'; // Cambia esto por la contraseña que quieras usar
 bcrypt.hash(password, 10, (err, hash) => {
